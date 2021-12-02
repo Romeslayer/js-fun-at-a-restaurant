05c52241596111337e2251cfb285d2a3fbf75aad
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-12-02 16:27:23 -0700

    complete meal.js activity

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  name = 'Delicious ' + name;
+  return name;
+}
 
+function createMenuItem(name, price, type) {
+  var item = {};
+  item.name = name;
+  item.price = price;
+  item.type = type;
+  return item;
+}
+
+function addIngredients(ingredient, arr) {
+if(!arr.includes(ingredient)) {
+  arr.push(ingredient);
+}
+}
+
+function formatPrice(price) {
+  return "$" + price;
+}
+
+function decreasePrice(price) {
+  return price - (price * .1) ;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var obj = {};
+  obj.title = title;
+  obj.ingredients = ingredients;
+  obj.type = menuItemType;
+
+  return obj;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
