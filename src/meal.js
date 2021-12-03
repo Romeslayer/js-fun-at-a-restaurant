52f21224a64083a7c5370c6806a32db35f2f5756
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-12-03 11:24:58 -0700

    correct syntax

--- src/meal.js
+++ src/meal.js
@@ -1,7 +1,7 @@
 function nameMenuItem(name) {
   name = 'Delicious ' + name;
   return name;
-}
+};
 
 function createMenuItem(name, price, type) {
   var item = {};
@@ -9,30 +9,29 @@ function createMenuItem(name, price, type) {
   item.price = price;
   item.type = type;
   return item;
-}
+};
 
 function addIngredients(ingredient, arr) {
-if(!arr.includes(ingredient)) {
+  if(!arr.includes(ingredient)) {
   arr.push(ingredient);
-}
-}
+  }
+};
 
 function formatPrice(price) {
-  return "$" + price;
-}
+  return '$' + price;
+};
 
 function decreasePrice(price) {
-  return price - (price * .1) ;
-}
+  return price - (price * .1);
+};
 
 function createRecipe(title, ingredients, menuItemType) {
   var obj = {};
   obj.title = title;
   obj.ingredients = ingredients;
   obj.type = menuItemType;
-
   return obj;
-}
+};
 
 
 module.exports = {
@@ -42,4 +41,4 @@ module.exports = {
   formatPrice,
   decreasePrice,
   createRecipe
-}
+};
--- src/order.js
+++ src/order.js
@@ -1,46 +1,49 @@
 function takeOrder(order, arr) {
-  if(arr.length < 3) {
-    arr.push(order)
+  if (arr.length < 3) {
+    arr.push(order);
   }
   return arr;
-}
+};
 
 function refundOrder(orderNumb, arr) {
 // Loop through each element in Array
   arr.forEach(function(element, index) {
     // Function checks if property orderNumber is the same as parameter orderNumb
-    if(element.orderNumber === orderNumb) {
+    if (element.orderNumber === orderNumb) {
       // If the statement above is true remove element of array
-      arr.splice(index, 1)
+      arr.splice(index, 1);
     }
   }
-)
-}
+  )
+};
 
 function listItems(arr) {
-  var string = "";
+  var string = '';
   // Loop through array and add the item value to string variable with a comma and space
   for(var i = 0; i < arr.length; i++) {
     string += `${arr[i].item}, `;
-  }
+  };
+
   //delete last space and comma from string
-  string = string.slice(0, -2)
+  string = string.slice(0, -2);
   return string;
-}
+};
 
 function searchOrder(arr, item) {
-  var boolean = false
+  var boolean = false;
   for(var i = 0; i < arr.length; i++) {
-    if(arr[i].item == item) {
+    if (arr[i].item === item) {
       boolean = true;
     }
+
   }
+
   return boolean;
-}
+};
 
 module.exports = {
   takeOrder,
   refundOrder,
   listItems,
   searchOrder
-}
+};
