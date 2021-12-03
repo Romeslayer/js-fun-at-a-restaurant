49fa5ee302e17488908038de802fdd849ffab754
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-12-02 17:02:38 -0700

    complete takeOrder function

--- src/order.js
+++ src/order.js
@@ -1,7 +1,18 @@
+function takeOrder(order, arr) {
+  if(arr.length < 3) {
+    arr.push(order)
+  }
+  return arr;
+}
+
+function refundOrder() {
+
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
