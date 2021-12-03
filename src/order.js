59d95cbd7bbad58817e7715a36104bce7023f6df
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-12-03 09:33:03 -0700

    complete refund orderFunction

--- src/order.js
+++ src/order.js
@@ -5,8 +5,16 @@ function takeOrder(order, arr) {
   return arr;
 }
 
-function refundOrder() {
-
+function refundOrder(orderNumb, arr) {
+// Loop through each element in Array
+  arr.forEach(function(element, index) {
+    // Function checks if property orderNumber is the same as parameter orderNumb
+    if(element.orderNumber === orderNumb) {
+      // If the statement above is true remove element of array
+      arr.splice(index, 1)
+    }
+  }
+)
 }
 
 
