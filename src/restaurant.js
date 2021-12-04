8d56d282345f08789e467b0c6b0ebbb3d0f9488f
DeNaje Ferguson <denaje.ferguson@gmail.com>
2021-12-03 23:09:22 -0700

    complete addMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,17 +1,37 @@
 function createRestaurant(name) {
-  var resturant = {};
-  resturant.name = name;
-  resturant.menus = {
+  var restaurant = {};
+  restaurant.name = name;
+  restaurant.menus = {
     breakfast: [],
     lunch: [],
     dinner: []
   };
 
-  return resturant;
+  return restaurant;
+}
+
+function addMenuItem(restaurant, menuItem) {
+  var restaurantMenuRef = restaurant.menus[menuItem.type]
+  if(restaurantMenuRef.length === 0) {
+    restaurantMenuRef.push(menuItem);
+    return
+  } else {
+    var boolean = true;
+    // Loop through array and check if array has an element with the same name property as menuItem
+    for (var i = 0; i < restaurantMenuRef.length; i++) {
+      if (restaurantMenuRef[i].name === menuItem.name) {
+        boolean = false;
+      }
+    }
+
+    if(boolean) {
+      restaurantMenuRef.push(menuItem);
+    }
+  }
 }
 
 module.exports = {
   createRestaurant,
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem
 }
